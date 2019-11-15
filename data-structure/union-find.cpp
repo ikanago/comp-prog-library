ec6abf0b5e22232d2c6fd098537ea61d535c4cff
YutaNagaoka <shirasu.okatin@gmail.com>
2019-11-15 12:50:47 +0900

    update: Implement size for Union-Find tree.

--- data-structure/union-find.cpp
+++ data-structure/union-find.cpp
@@ -1,23 +1,20 @@
 struct UnionFind {
-    vector<int> parent, rank;
+    vector<int> _parent;
+    vector<int> _size;
 
-    explicit UnionFind(int n) : parent(n) {
-        parent.resize(n);
-        rank.resize(n, 0);
+    explicit UnionFind(int n) : _parent(n), _size(n, 1LL) {
         for (int i = 0; i < n; i++)
-            parent[i] = i;
+            _parent[i] = i;
     }
 
     int root(int x) {
-        if (parent[x] == x)
+        if (_parent[x] == x)
             return x;
-        return parent[x] = root(parent[x]);
+        return _parent[x] = root(_parent[x]);
     }
 
-    bool is_same(int x, int y) {
-        int rx = root(x);
-        int ry = root(y);
-        return root_x == root_y;
+    bool same(int x, int y) {
+        return root(x) == root(y);
     }
 
     void unite(int x, int y) {
@@ -25,11 +22,13 @@ struct UnionFind {
         int ry = root(y);
         if (rx == ry)
             return;
-        if (rank[x] < rank[y])
-            parent[rx] = ry;
-        else {
-            parent[ry] = rx;
-            rank[rx] += (rank[x] == rank[y]);
-        }
+        if (size(rx) < size(ry))
+            swap(rx, ry);
+        _parent[ry] = rx;
+        _size[rx] += _size[ry];
+    }
+
+    int size(int x) {
+        return _size[x];
     }
 };

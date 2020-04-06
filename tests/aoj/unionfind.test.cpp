1ddc4e57bc934c66f10d16d43979cc5955d6b5d8
ikanago <28985004+ikanago@users.noreply.github.com>
2020-04-06 14:16:52 +0900

    🛠 Add test for CI

--- tests/aoj/unionfind.test.cpp
+++ tests/aoj/unionfind.test.cpp
@@ -1,3 +1,7 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/all/DSL_1_A"
+#include <iostream>
+using namespace std;
+
 #include <vector>
 
 struct UnionFind {
@@ -40,3 +44,20 @@ struct UnionFind {
         return _size[root(x)];
     }
 };
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+    UnionFind trees(n);
+    for (size_t i = 0; i < q; i++)
+    {
+        int c, x, y;
+        cin >> c >> x >> y;
+        if (c == 0) {
+            trees.unite(x, y);
+        }
+        else {
+            cout << (trees.same(x, y) ? 1 : 0) << "\n";
+        }
+    }
+}

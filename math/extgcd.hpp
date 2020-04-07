858e5c2ed137cd4a0f9143dac7fe8f520fb57cac
ikanago <28985004+ikanago@users.noreply.github.com>
2020-04-07 22:06:46 +0900

    ✨ Test for Extended Euclidean algorithm

--- math/extgcd.hpp
+++ math/extgcd.hpp
@@ -1,13 +1,13 @@
 // 拡張ユークリッドの互除法で `ax + by = GCD(a, b)` の解 `(x, y)` を求める
 template <typename T>
-T extGCD(T a, T b, T& x, T& y)
+T extgcd(T a, T b, T& x, T& y)
 {
     if (b == 0) {
         x = 1;
         y = 0;
         return a;
     }
-    T d = extGCD(b, a % b, y, x);
+    T d = extgcd(b, a % b, y, x);
     y -= a / b * x;
     return d;
 }
---
+++ tests/aoj/math_extgcd.test.cpp
@@ -0,0 +1,10 @@
+#define PROBLRM "https://onlinejudge.u-aizu.ac.jp/courses/library/6/NTL/all/NTL_1_E"
+#include "../../math/extgcd.hpp"
+#include <iostream>
+
+int main() {
+    int a, b, x, y;
+    std::cin >> a >> b;
+    extgcd<int>(a, b, x, y);
+    std::cout << x << " " << y << std::endl;
+}
